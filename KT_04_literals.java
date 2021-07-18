6ea7fbe49d96bdc4ac92ff4565fc5910a3384da8
Karn-Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2021-07-18 15:23:09 +0530

    Add files via upload

---
+++ KT_04_literals.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class KT_04_literals {
+    public static void main(String[] args) {
+        int age = 21;
+        char ch = 'A';
+        long l1 = 45_555_555_555L;
+        float f1 = 5.6f;
+        double d1 = 4.66D;
+        System.out.println(age);
+String str  = "Karn Tiwari";
+System.out.println(str);
+    }
+}
