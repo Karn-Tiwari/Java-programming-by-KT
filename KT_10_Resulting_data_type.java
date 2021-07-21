7c917398cd07ec02ccc0cd7e39d616cf989cbcc5
Karn-Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2021-07-21 09:55:18 +0530

    Add files via upload

---
+++ KT_10_Resulting_data_type.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class KT_10_Resulting_data_type {
+    public static void main(String[] args) {
+       /* byte x = 5;
+        int y =  8;
+        short z = 6;
+        int a = y+z;
+        float b = 6.45f + x;
+        System.out.println(b);*/
+
+        //Increment and Decrement Operator
+        int i = 65;
+        System.out.println(++i); // pehle increment hoga uske baad evaluate hoga
+        int j = 56;
+        System.out.println(j++); // pehle evaluate ho uske baad increment hoga
+    }
+}
