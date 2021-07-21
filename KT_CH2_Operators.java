d8200e9f0c2d9e39244dafda8d34c6a6bb5d6303
Karn-Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2021-07-21 09:56:08 +0530

    Add files via upload

---
+++ KT_CH2_Operators.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class KT_CH2_Operators {
+    public static void main(String[] args) {
+        int a = 6;
+        
+//        int b = 8%a; //% ye reminder deta hai
+        int b  = 9;
+        b *= 3;
+        System.out.println(b); // module operator
+        System.out.println(64<7);// comparision operatop
+        System.out.println(64>10 && 64<56); // logical operator
+        System.out.println(64>10 || 64<56);
+    }
+}
