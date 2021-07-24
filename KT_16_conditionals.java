b3a8748a113de215ba2e355116b69ae93870cfea
Karn-Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2021-07-24 23:56:27 +0530

    Add files via upload

---
+++ KT_16_conditionals.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class KT_16_conditionals {
+    public static void main(String[] args) {
+int age  = 24;
+boolean cond = (age>=18);
+if (cond)
+{
+    System.out.println("yes man,you can drive..!");
+}
+else{
+    System.out.println("No man, you cannot drive..!");
+}
+    }
+}
