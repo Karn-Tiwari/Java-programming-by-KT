acc71526f12b04488e12e969f6635968eb0e4f6a
Karn-Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2021-07-25 19:11:22 +0530

    Add files via upload

---
+++ KT_17_logical.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class KT_17_logical {
+    public static void main(String[] args){
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+        if(a&&b&&c)
+        {
+            System.out.println("yes");
+
+        }
+        else{
+            System.out.println("no");
+
+        }
+    }
+
+}
+// or operator || by which if anyone will true then result is also is to be true
